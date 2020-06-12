3e2fcc90afb9e7db210c6611f06873bd1425846c
Adam Niedzwiedz <niedzwiedz.adam@gmail.com>
2020-06-12 23:09:29 +0200

    Updated test

--- sht31/test.py
+++ sht31/test.py
@@ -9,9 +9,8 @@ if __name__ == "__main__":
         sys.exit(1)
     try:
         while True:
-            temp = sht31.read_temperature()
+            temp, hum = sht31.read_temperature_humidity()
             print('Temp             = {0:0.3f} deg C'.format(temp))
-            hum = sht31.read_humidity()
             print('Humidity         = {0:0.2f} %'.format(hum))
             sleep(5.0)
     except KeyboardInterrupt:
